71370d2fb12bbaa5c5682c6b3eb96f3e7700257b
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2025-07-25 15:32:12 +0200

    feat: ✨ Initialisation project

---
+++ src/test/java/com/hb/cda/tricount_api/TricountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.cda.tricount_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
